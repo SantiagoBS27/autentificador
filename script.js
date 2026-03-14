438c90e1f05229278a0541496b1e5a0d9b968db7
SantiagoBS27 <166900142+SantiagoBS27@users.noreply.github.com>
2026-03-14 00:01:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+class Usuario {
+    constructor(nombre, email, password){
+        this.nombre = nombre; 
+        this.email = email; 
+        this.password = password; 
+    }
+}
+
+function register(name, email, pass){
+    let user = new Usuario(name, email, pass); 
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    users.push(user); 
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+function verify(name, email, pass){
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    for(let user of users){
+        if(user.email == email){
+            alert("email already exists"); 
+            return; 
+        }
+    }
+    register(name, email, pass); 
+    window.location.href = "login.html";
+}
+
+function registrarUsuario(){
+    let nombre = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    
+    if(nombre.length < 6){
+        alert("Name must be at least 6 characters"); 
+        return; 
+    }
+    if(!email.includes("@")){
+        alert("Invalid email"); 
+        return; 
+    }
+    if(password.length < 6){
+    alert("Password must be at least 6 characters"); 
+    return; 
+    }
+
+    verify(nombre, email, password);
+
+}
